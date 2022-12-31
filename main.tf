f820d267ae9426a216c5dc5f0b22be884ec49552
esioDesynC <esio_paiva@hotmail.com>
2022-12-31 01:10:21 -0300

    Adiciona as pastas a organização do GCP

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "barbero-devops-iac"
+  project = "desync-shared-devops-prod-Iac"
   region  = "us-central1"
   zone    = "us-central1-c"
   credentials = "${file("serviceaccount.yaml")}"
@@ -7,17 +7,12 @@ provider "google" {
 
 resource "google_folder" "Financeiro" {
   display_name = "Financeiro"
-  parent       = "organizations/540829645030"
+  parent       = "organizations/674618817316"  #[ insira o numero da sua org]
 }
 
 resource "google_folder" "SalesForce" {
   display_name = "SalesForce"
   parent       = google_folder.Financeiro.name
-}
-
-resource "google_folder" "Desenvolvimento" {
-  display_name = "Desenvolvimento"
-  parent       = google_folder.SalesForce.name
 }
 
 resource "google_folder" "Producao" {
@@ -26,19 +21,30 @@ resource "google_folder" "Producao" {
 }
 
 
-resource "google_project" "barberosa2-salesforce-dev" {
+resource "google_project" "desync-salesforce-prod" {
   name       = "SalesForce-Dev"
   project_id = "barberosa2-salesforce-dev"
-  folder_id  = google_folder.Desenvolvimento.name
+  folder_id  = google_folder.Producao.name
   auto_create_network=false
   billing_account = "018973-A8340F-83D8E5"
 
 }
 
-# resource "google_project" "barberosa-salesforce-prod" {
-#   name       = "SalesForce-Prod"
-#   project_id = "barberosa-salesforce-prod"
-#   folder_id  = google_folder.Producao.name
-#   auto_create_network=false
-#   billing_account = "01B2C4-70459E-F53446"
-# }
+resource "google_folder" "ContaAzul" {
+  display_name = "ContaAzul"
+  parent       = google_folder.Financeiro.name
+}
+
+resource "google_folder" "Producao" {
+  display_name = "Producao"
+  parent       = google_folder.ContaAzul.name
+}
+
+resource "google_project" "desync-conta_azul-prod" {
+  name       = "SalesForce-Dev"
+  project_id = "barberosa2-salesforce-dev"
+  folder_id  = google_folder.Producao.name
+  auto_create_network=false
+  billing_account = "018973-A8340F-83D8E5"
+  
+  }
